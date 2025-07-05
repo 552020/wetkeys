e7fda9cdda5407430455fe11b909553e26d03aef
552020 <stefanolombardo@posteo.de>
2025-07-05 20:10:06 +0200

    chore: add packages and implement login

--- src/vtk_frontend/src/main.jsx
+++ src/vtk_frontend/src/main.jsx
@@ -2,9 +2,23 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./style.css";
+import { WagmiProvider } from "wagmi";
+import { baseSepolia } from "wagmi/chains";
+import { RainbowKitProvider, getDefaultConfig } from "@rainbow-me/rainbowkit";
+import "@rainbow-me/rainbowkit/styles.css";
+
+const config = getDefaultConfig({
+  appName: "VTK App",
+  chains: [baseSepolia],
+  projectId: "YOUR_WALLETCONNECT_PROJECT_ID", // Replace with your WalletConnect Project ID
+});
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <WagmiProvider config={config}>
+      <RainbowKitProvider>
+        <App />
+      </RainbowKitProvider>
+    </WagmiProvider>
   </React.StrictMode>
 );
